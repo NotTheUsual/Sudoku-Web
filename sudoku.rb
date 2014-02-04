fe4c8b2e15b87c6145471437bd8c66009b74aff4
James Hunter <james.hunter@me.com>
2014-02-04 13:54:51 +0000

    Enabled sessions (for storing solution)

--- sudoku.rb
+++ sudoku.rb
@@ -2,18 +2,28 @@ require 'sinatra'
 require_relative './lib/sudoku'
 require_relative './lib/cell'
 
+enable :sessions
+
 def random_sudoku
-  # We're using 9 numbers, 1 to 9, and 72 zeros as an input
-  # it's obvious there may be no clashes as all numbers are unique
   seed = (1..9).to_a.shuffle + Array.new(81-9,0)
   sudoku = Sudoku.new(seed.join)
-  # Then we solve this (really hard!) sudoku
   sudoku.solve!
-  # and give the output to the view as an array of chars
   sudoku.to_s.chars
 end
 
+def puzzle(sudoku)
+  # This method removes some digits form the puzzle to create a solution
+  # This method is yours to implement
+end
+
 get '/' do
-  @current_solution = random_sudoku
+  sudoku = random_sudoku
+  session[:sudoku] = sudoku
+  @current_solution = puzzle(sudoku)
+  erb :index
+end
+
+get '/solution' do
+  @current_solution = session[:sudoku]
   erb :index
 end
